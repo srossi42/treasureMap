c9f5f14cacbdef89cd1db67a9bbe5607a37c0700
srossi42 <33451464+srossi42@users.noreply.github.com>
2023-10-21 20:01:26 +0200

    Delete calculPosition file

--- src/utils/instanceCreation.ts
+++ src/utils/instanceCreation.ts
@@ -1,5 +1,22 @@
 import {CustomMap} from "../class/Map";
 import {Adventurer} from "../class/Adventurer";
+import {Orientation} from "../enumsAndConstants/orientation";
+
+
+function getOrientation(orientationStr: string): Orientation | undefined {
+    let orientation: Orientation | undefined = undefined;
+    if (orientationStr === 'N') {
+        orientation = Orientation.N;
+    } else if (orientationStr === 'S') {
+        orientation = Orientation.S;
+    } else if (orientationStr === 'E') {
+        orientation = Orientation.E;
+    } else if (orientationStr === 'O') {
+        orientation = Orientation.O;
+    }
+    return orientation;
+
+}
 
 export function createInstanceFromLine(line: string): CustomMap | Adventurer | null {
     if (line.startsWith('#')) {
@@ -14,10 +31,12 @@ export function createInstanceFromLine(line: string): CustomMap | Adventurer | n
             const name: string = line.split(' - ')[1];
             const xPosition: number = parseInt(line.split(' - ')[1]);
             const yPosition: number = parseInt(line.split(' - ')[2]);
-            const orientation: string = line.split(' - ')[3];
+            const orientation: Orientation | undefined = getOrientation(line.split(' - ')[3]);
             const movementList: string = line.split(' - ')[4];
             const treasureCount: number = 0;
+
             return new Adventurer(name, xPosition, yPosition, orientation, movementList, treasureCount);
+
         }
     }
     return null;
