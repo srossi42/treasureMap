ceff3b4bcfc63efd66df5456a057199db0740f74
srossi42 <savinien.rossi@solypse.com>
2023-10-23 18:44:39 +0200

    Utils: Add and refacto functions

--- src/utils/instanceCreation.ts
+++ src/utils/instanceCreation.ts
@@ -1,6 +1,8 @@
 import {CustomMap} from "../class/Map";
 import {Adventurer} from "../class/Adventurer";
 import {Orientation} from "../enumsAndConstants/orientation";
+import {Mountain} from "../class/Mountain";
+import {Treasure} from "../class/Treasure";
 
 
 function getOrientation(orientationStr: string): Orientation | undefined {
@@ -18,26 +20,36 @@ function getOrientation(orientationStr: string): Orientation | undefined {
 
 }
 
-export function createInstanceFromLine(line: string): CustomMap | Adventurer | null {
+export function createInstanceFromLine(line: string, separator = ' - '): CustomMap | Adventurer | Mountain | Treasure | null {
     if (line.startsWith('#')) {
         // It's a comment line, do nothing
         return null;
     } else {
         if (line.startsWith('C')) {
-            const width: number = parseInt(line.split(' - ')[1]);
-            const height: number = parseInt(line.split(' - ')[2]);
+            const width: number = parseInt(line.split(separator)[1]);
+            const height: number = parseInt(line.split(separator)[2]);
             return new CustomMap(width, height);
         } else if (line.startsWith('A')) {
-            const name: string = line.split(' - ')[1];
-            const xPosition: number = parseInt(line.split(' - ')[1]);
-            const yPosition: number = parseInt(line.split(' - ')[2]);
-            const orientation: Orientation | undefined = getOrientation(line.split(' - ')[3]);
-            const movementList: string = line.split(' - ')[4];
+            const name: string = line.split(separator)[1];
+            const xPosition: number = parseInt(line.split(separator)[2]);
+            const yPosition: number = parseInt(line.split(separator)[3]);
+            const orientation: Orientation | undefined = getOrientation(line.split(separator)[4]);
+            const movementList: string = line.split(separator)[5];
             const treasureCount: number = 0;
 
             return new Adventurer(name, xPosition, yPosition, orientation, movementList, treasureCount);
 
+        } else if (line.startsWith('M')) {
+            const xPosition: number = parseInt(line.split(separator)[1]);
+            const yPosition: number = parseInt(line.split(separator)[2]);
+            return new Mountain(xPosition, yPosition);
         }
+     else if (line.startsWith('T')) {
+        const xPosition: number = parseInt(line.split(separator)[1]);
+        const yPosition: number = parseInt(line.split(separator)[2]);
+        const count: number = parseInt(line.split(separator)[3]);
+        return new Treasure(xPosition, yPosition, count);
+    }
     }
     return null;
 }
