e683c90802f64b47311df01ed6d60f2bd25910d2
srossi42 <savinien.rossi@solypse.com>
2023-10-23 18:44:19 +0200

    [Treasure] Fix constructor, add basic functions

--- src/class/Treasure.ts
+++ src/class/Treasure.ts
@@ -1,14 +1,22 @@
 export class Treasure {
     private _count: number;
+    private _x: number;
+    private _y: number;
 
-    constructor(count: number = 0) {
+    constructor(x: number, y: number, count: number = 0) {
+        this._x = x;
+        this._y = y;
         if (count >= 0) {
             this._count = count;
         } else {
             throw new Error('Treasure count must be positive or equal to 0');
         }
+
     }
 
+    getPosition(): {x: number, y: number} {
+        return {x: this._x, y: this._y};
+    }
     getCount(): number {
         return this._count;
     }
