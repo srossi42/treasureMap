58c1f16e0bb7e55c6c3c9615bf5cd8fb03f9d41a
srossi42 <savinien.rossi@solypse.com>
2023-10-23 18:43:17 +0200

    [Mountain] Add a mountain class for Cell content

--- src/class/Mountain.ts
+++ src/class/Mountain.ts
@@ -1,36 +1,13 @@
-import {Adventurer} from "./Adventurer";
-import {Treasure} from "./Treasure";
-import {CustomMap} from "./Map";
+export class Mountain {
 
-export class Game {
-    private _adventurerList: Adventurer [];
-    private _treasureList: Treasure [];
-    private map: CustomMap | null;
-
-
-    constructor(map?: CustomMap, adventurerList: Adventurer[] = [], treasureList: Treasure[] = []) {
-        this._adventurerList = adventurerList;
-        this._treasureList = treasureList;
-        this.map = map ? map : null;
+    private _x: number;
+    private _y: number;
+    constructor(x: number, y: number) {
+        this._x = x;
+        this._y = y;
     }
 
-    addAdventurer(adventurer: Adventurer) {
-        if (this._adventurerList.findIndex((adv) => adv.getName() === adventurer.getName()) !== -1) {
-            throw new Error('Adventurer already exists');
-        } else {
-            this._adventurerList.push(adventurer);
-        }
+    getPosition(): {x: number, y: number} {
+        return {x: this._x, y: this._y};
     }
-
-    hasMap(): boolean {
-        return this.map !== null;
-    }
-
-    getMap() {
-        return this.map;
-    }
-    setMap(map: CustomMap) {
-        this.map = map;
-    }
-
 }
